a53a270d8f61bf2dacac1e768916f090396e67ad
nehachauhan4423 <nehachauhan4423@gmail.com>
2024-12-18 21:44:26 +0530

    Node

---
+++ 3-Local Modules/calcmodule.js
@@ -0,0 +1,25 @@
+console.log("Hiee Node JS");
+
+const sum = (a, b) => {
+    return a + b;
+}
+const min = () => {
+    a = 10, b = 5
+    return a - b;
+}
+const mul = (a, b) => {
+    return a * b;
+}
+const div = () => {
+    a= 80 , b=4
+    return a/b;
+}
+const arr1 = [10,30,78,40]
+
+module.exports = {
+    sum,
+    min,
+    mul,
+    div,
+    arr1
+}
---
+++ 3-Local Modules/index.js
@@ -0,0 +1,8 @@
+const { sum, min,mul,div,arr1 } = require("./calcmodule");
+console.log(`Sum:-${sum(10,5)}`);
+console.log(`Sub:-${min()}`);
+console.log(`Mul:-${mul(10,5)}`);
+console.log(`Div:-${div()}`);
+console.log(arr1);
+
+
