ed941bdb16097ab36c0ab7fa634cf04087964cfa
nehachauhan4423 <nehachauhan4423@gmail.com>
2024-12-19 10:37:38 +0530

    Node

---
+++ 4-Create Server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 8000;
+const server = http.createServer((req,res)=>{
+    res.write(`<h1>Hello</h1>`);
+    res.end();
+})
+server.listen(port,(err) => {
+   if (!err) {
+    console.log(`server is strat on port:-${port}`);
+    console.log(`${port}`);
+}
+});
