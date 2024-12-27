f82b5df5935c744f36e11f7d99806e70ff32c95f
nehachauhan4423 <nehachauhan4423@gmail.com>
2024-12-27 17:29:46 +0530

    Node

--- PR-1-Create custom server using HTTP/Index.js
+++ PR-1-Create custom server using HTTP/Index.js
@@ -1,17 +1,41 @@
 const http = require("http");
 const port = 8001;
-const fs= require("fs");
-const server = http.createServer((req,res)=>{
-    let fileName= "";
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+    let fileName = "";
     switch (req.url) {
         case "/":
-            fileName ="./home.html"
+            fileName = "./home.html"
             break;
         case "/about":
             fileName = "./about.html"
             break;
+        case "/account":
+            fileName = "./account.html"
+            break;
+        case "/blog":
+            fileName = "./blog.html"
+            break;
+        case "/cart":
+            fileName = "./cart.html"
+            break;
+        case "/login":
+            fileName = "./login.html"
+            break;
+        case "/order":
+            fileName = "./order.html"
+            break;
+        case "/policy":
+            fileName = "./policy.html"
+            break;
+        case "/register":
+            fileName = "./register.html"
+            break;
+        case "/shoppinng":
+            fileName = "./shopping.html"
+            break;
     }
-    fs.readFile(fileName,(err,pagename)=>{
+    fs.readFile(fileName, (err, pagename) => {
         if (err) {
             console.log(`file is not found`);
             return false;
@@ -19,7 +43,7 @@ const server = http.createServer((req,res)=>{
         res.end(pagename)
     })
 })
-server.listen(port,(err)=>{
+server.listen(port, (err) => {
     if (!err) {
         console.log(`${port}`);
     }
