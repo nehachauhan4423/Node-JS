8ef96f20ed9347000133361a70a86a5fd1385d05
nehachauhan4423 <nehachauhan4423@gmail.com>
2025-02-20 20:00:42 +0530

    Node

--- 19-Admin Pannel/controller/authController.js
+++ 19-Admin Pannel/controller/authController.js
@@ -1,66 +1,153 @@
 const UserModel = require('../models/UserModel');
-//nodemailer add krvanu baki che
-const loginPage = (req,res) => {
+
+const nodemailer = require('nodemailer');
+
+const loginPage = (req, res) => {
     if (res.locals?.users) {
         return res.redirect('/dashboard');
     }
     return res.render('login');
 }
 
-const registerPage = (req,res) => {
+const registerPage = (req, res) => {
     return res.render('register');
 }
 
-const dashboardPage = (req,res) => {
+const dashboardPage = (req, res) => {
     return res.render('dashboard');
 }
 
-const registerUser = async (req,res) => {
-    try{
-        const {name,email,password,cpassword} = req.body;
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, cpassword } = req.body;
         if (password == cpassword) {
             let user = await UserModel.create({
-                name : name,
-                email : email,
-                password : password
+                name: name,
+                email: email,
+                password: password
             })
             console.log("USER REGISTER");
             return res.redirect('/');
-        } else{
+        } else {
             console.log("PASSWORD AND CONFIG PASSWORD NOT MATCH");
             return res.redirect('/register');
         }
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return false;
     }
 }
 
-const loginUser = async(req,res) => {
-    try{
+const loginUser = async (req, res) => {
+    try {
         return res.redirect('/dashboard');
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return false;
     }
 }
 
-// const logoutUser = (req,res) => {
-//     req.logout((err)=> {
-//         if (err) {
-//             console.log(err);
-//             return false;
-//         }
-//         return res.redirect('/');
-//     })
-// }
+const logoutUser = (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        return res.redirect('/');
+    })
+}
 
+const forgotPassword = async (req, res) => {
+    try {
+        const { useremail } = req.body;
+        let user = await UserModel.findOne({ email: useremail });
+        if (!user) {
+            console.log("EMAIL AND PASS NOT VALID");
+            return res.redirect('/');
+        }
+        let opt = Math.floor(Math.random() * 100000);
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'nehachauhan4423@gmail.com',
+                pass: 'yourpassword' //err
+            }
+        });
 
+        var mailOptions = {
+            from: 'nehachauhan4423@gmail.com',
+            to: useremail,
+            subject: 'Forgot Password',
+            text: `<h1 style='color:green'>Your Otp :- ${opt}</h1>`,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                let userotp = {
+                    otp: otp,
+                    email: useremail
+                }
+                res.cookie('userotp', userotp)
+                console.log('Email sent: ' + info.response);
+                return res.redirect('/otp');
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return false
+    }
+}
+
+const otp = (req, res) => {
+    return res.render('otp');
+}
+const postOtp = (req, res) => {
+    let otp = req.body.otp;
+    let userotp = req.cookies.userotp?.otp;
+    if (otp == userotp) {
+        return res.redirect('/newpassword')
+    } else {
+        console.log("OTP IS NOT MATCH");
+        return res.redirect('/opt')
+    }
+}
+
+const newPasswordPage = (req, res) => {
+    return res.render('newpassword');
+}
+
+const postNewPassword = async (req, res) => {
+    try {
+        const { newpassword, cpassword } = req.body;
+        if (newpassword == cpassword) {
+            let useremail = req.cookies.userotp?.email;
+            await UserModel.findOneAndUpdate({ email: useremail }, {
+                password: newpassword
+            })
+            res.clearCookie('userotp');
+            return res.redirect('/');
+        } else {
+            console.log("NEW PASS AND CONFIG PASS NOT VALID");
+            return res.redirect('/newpassword')
+        }
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
 
 module.exports = {
     loginPage,
     registerPage,
     dashboardPage,
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser,
+    forgotPassword,
+    otp,
+    postOtp,
+    newPasswordPage,
+    postNewPassword
 }
--- 19-Admin Pannel/routes/authRoute.js
+++ 19-Admin Pannel/routes/authRoute.js
@@ -2,18 +2,22 @@ const express = require('express');
 
 const routes = express.Router();
 
-const { loginPage, registerPage, dashboardPage, registerUser, loginUser } = require('../controller/AuthController');
+const { loginPage, registerPage, dashboardPage, registerUser, loginUser, logoutUser, forgotPassword, postOtp, newPasswordPage, postNewPassword, otp } = require('../controller/AuthController');
 
 const passport = require('passport')
 
 
 routes.get('/',loginPage)
 routes.get('/register',registerPage)
-routes.get('/dashboard',dashboardPage)
+routes.get('/dashboard',passport.checkUser,dashboardPage)
 routes.post('/registeruser',registerUser)
-routes.post('/loginuser',loginUser)
-
+routes.post('/loginuser',passport.authenticate('local',{failureRedirect:'/'}),loginUser)
+routes.get('/logout',logoutUser)
+routes.post('/forgotpassword',forgotPassword)
+routes.get('/otp',otp);
+routes.post('/postotp',postOtp)
+routes.get('/newpassword',newPasswordPage)
+routes.get('/postnewpassword',postNewPassword)
 
 
 module.exports = routes;
-
