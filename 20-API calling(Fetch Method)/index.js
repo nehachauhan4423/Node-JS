3b0050079a52908bba1b615f30d533028ffcd0ea
nehachauhan4423 <nehachauhan4423@gmail.com>
2025-03-06 08:16:16 +0530

    Node

--- 20-API calling(Fetch Method)/index.js
+++ 20-API calling(Fetch Method)/index.js
@@ -4,11 +4,26 @@ const app = require(app);
 
 const port = 9080;
 
+
+app.get('/users',(req,res)=> {
+    return res.status(200).json({
+        success : true,
+        message : 'User Fetch',
+        users : [
+            {id:1,name : "kunjal",phone : 56418},
+            {id:2,name : "janavi",phone : 12974},
+            {id:3,name : "brijal",phone : 95317},
+            {id:4,name : "bumika",phone : 93176},
+            {id:5,name : "dhruvi",phone : 97315}
+        ]
+    })
+})
+
 app.listen(port,(err)=> {
     if (err) {
         console.log(err);
         return false
     }
-    console.log(`server is start on port ${port321}`);
+    console.log(`server is start on port ${port}`);
     
 })
